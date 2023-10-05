fda8171f6dab1b8a9e2995b20e94fbff5fac2412
juliasurzhenko <116028597+juliasurzhenko@users.noreply.github.com>
2023-10-05 17:21:17 +0300

    lab 2

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -1,125 +1,144 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(GalleryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class GalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AlbumListPage(),
+      routes: {
+        '/album': (context) {
+          final album = ModalRoute.of(context)!.settings.arguments as Album;
+          return AlbumPhotosPage(album: album);
+        },
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AlbumListPage extends StatelessWidget {
+  final List<Album> albums = [
+    Album("Minions 1", [
+      'assets/img1.jpeg',
+      'assets/img2.jpg',
+      'assets/img1.jpeg',
+      'assets/img2.jpg',
+      'assets/img1.jpeg',
+      'assets/img2.jpg',
+      'assets/img1.jpeg',
+      'assets/img2.jpg',
+    ]),
+    Album("Minions 2", [
+      'assets/img2.jpg',
+      'assets/img1.jpeg',
+      'assets/img2.jpg',
+    ]),
+  ];
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text("Albums"),
+      ),
+      body: ListView.builder(
+        itemCount: albums.length,
+        itemBuilder: (context, index) {
+          final album = albums[index];
+          return ListTile(
+            title: Text(album.name),
+            leading: Image.asset(
+              album.photos[0], // Display the first photo as a thumbnail
+              width: 50, // Adjust as needed
+              height: 50, // Adjust as needed
+            ),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/album',
+                arguments: album,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AlbumPhotosPage extends StatefulWidget {
+  final Album album;
 
-  void _incrementCounter() {
+  AlbumPhotosPage({required this.album});
+
+  @override
+  _AlbumPhotosPageState createState() => _AlbumPhotosPageState();
+}
+
+class _AlbumPhotosPageState extends State<AlbumPhotosPage> {
+  int _currentIndex = 0;
+
+  void _previousPhoto() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_currentIndex > 0) {
+        _currentIndex--;
+      }
+    });
+  }
+
+  void _nextPhoto() {
+    setState(() {
+      if (_currentIndex < widget.album.photos.length - 1) {
+        _currentIndex++;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.orange,
+        title: Text(widget.album.name),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Image.asset(
+                widget.album.photos[_currentIndex],
+                width: 300, // Adjust as needed
+                height: 300, // Adjust as needed
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: _previousPhoto,
+              ),
+              Text("${_currentIndex + 1} / ${widget.album.photos.length}"),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: _nextPhoto,
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Album {
+  final String name;
+  final List<String> photos;
+
+  Album(this.name, this.photos);
 }
